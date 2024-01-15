e23fd9e942c51a5d91848747523bff0ce7828f3f
wiwi farel <wfarel.t@gmail.com>
2024-01-15 19:30:07 -0400

    Add product Images to homepage

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    products = Product.objects.all()
+    message = 'Hello World'
+    print(products)
+    return render(request, 'home.html', {'products': products, 'message': message})
